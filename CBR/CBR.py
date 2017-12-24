bc12d2b3a63563c7ba4e8034870a915eb4d06a4a
BauyrzhanOspan <dr.cleverest@gmail.com>
2017-12-24 17:04:48 +0000

    Wiki about CBR and Main functions. Comments.

--- CBR/CBR.py
+++ CBR/CBR.py
@@ -12,21 +12,27 @@ import pandas as pd
 import scipy as sp
 from scipy.spatial.distance import mahalanobis
 
+# Connect to db
 con = pymysql.connect(host='0.0.0.0', unix_socket='/tmp/mysql.sock', user="root", passwd="123", db='virtass')
 cur = con.cursor(pymysql.cursors.DictCursor)
 
+# Put coefficients of reasons and preferences
 prefList = {"energy": 0, "entertainment": 1, "food": 2, "health": 3, "security": 4, "work": 5}
 usersList = {"adult": 0.6, "young": 0.3, "elder": 0.6}
 
 
+# Function that loads cases and preferences
 def load():
     with con:
         cur.execute('SELECT * FROM cases')
         casesRaw = list(cur.fetchall())
         cur.execute('SELECT * FROM preferences')
         prefRaw = list(cur.fetchall())
+
+        # For each element of table is stored into list of training and test sets
         for k in range(len(casesRaw)):
             for l in range(len(prefRaw)):
+                # Each of the reason is parsed inside global variable and coefficients are put
                 if casesRaw[k]["usertypeIN"] == prefRaw[l]["usertype"]:
                     try:
                         n = float(prefRaw[l][str(casesRaw[k]["reasonIN"])])
@@ -39,17 +45,20 @@ def load():
                         casesRaw[k]["reasonOUT"] = n / 10
                     except KeyError:
                         n = 10
+            # Put coefficients from global variables to lists
             try:
                 casesRaw[k]["usertypeIN"] = usersList[casesRaw[k]["usertypeIN"]]
                 casesRaw[k]["usertypeOUT"] = usersList[casesRaw[k]["usertypeOUT"]]
             except KeyError:
                 n = 10
-
+    # Creating training and test set and return it
     training = casesRaw
     test = list(casesRaw[:])
+    print(test[0])
     return training, test
 
 
+# Pure CBR, calculating Mahalanobis distance and find nearest case and return it back
 def specy(covmx, test):
     training, f = load()
     trainingRaw = []
@@ -73,7 +82,7 @@ def specy(covmx, test):
     for k in range(len(training)):
         trains = [training[k]["reasonIN"], training[k]["reasonOUT"],
                   training[k]["usertypeIN"], training[k]["usertypeOUT"]]
-        distance = mahalanobis(trains, tests, invcovmx)
+        distance = mahalanobis(trains, tests, invcovmx)  # Calculating Mahalanobis distance
         distances.append([k, distance])
 
     classVotes = {}
@@ -94,6 +103,7 @@ def specy(covmx, test):
     return response
 
 
+# Calculate Accuracy of genome by compaing its values with predefined outputs, return accuracy percentage
 def Accuracy(weights):
     training, test = load()
     acc = 0
@@ -105,52 +115,57 @@ def Accuracy(weights):
     return (acc / len(test)) * 100.0
 
 
+# Fun starts here, this is genetic algorithm to find best Mahalanobis matrix
 def train():
     training, f = load()
     for el in range(len(training)):
         del (training[el]["output"])
         del (training[el]["id"])
+
+    # Creating Mahalanobis Matrix of zeros and store it in genome
     training1 = [{'usertypeIN': 0.0, 'reasonOUT': 0.0, 'usertypeOUT': 0.0, 'reasonIN': 0.0},
                  {'usertypeIN': 0.0, 'reasonOUT': 0.0, 'usertypeOUT': 0.0, 'reasonIN': 0.0}]
     df = pd.DataFrame(training1)
     covmx = df.cov()
     genome = covmx.round(3)
+
+    # Calculating time of execution
     start_time = time.time()
     king = {"Epoch": 0, "Genome": genome, "Accuracy": float(Accuracy(genome))}
     speed = time.time() - start_time
-    Epos = 100000
-    Deep = 1
+    Epos = 100000  # Defining number of epochs in training algorithm
+    Deep = 1  # Deepness of the GA, don`t change
     print("Number of epochs is " + str(Epos))
-    execution_time = 1.1 * speed * Epos * Deep * int(100)
+    execution_time = 1.1 * speed * Epos * Deep * int(100)  # Execution time of the GA
     print("Estimated execution time is " + str(datetime.timedelta(seconds=int(execution_time))))
     print("Estimated execution time for one epoch is " + str(
         datetime.timedelta(seconds=int(execution_time / 100000))))
     print("=======================================================================")
     print("Starting evolutional algorithm: ")
-    bb = 0
-    # Write the head of file
+
+    # Write the head of log file
     filename = "kings.txt"
     with open(filename, 'a') as out:
         str1 = "Number of epochs is " + str(Epos) + "\n" + "Deepness of the analysis is " + str(Deep) + "\n" + \
                "Estimated execution time is " + str(datetime.timedelta(seconds=int(execution_time))) + "\n " + \
                "Genetic algorithms optimisation for KNN with preference weights" + "\n" + "=" * 20 + "\n"
         out.write(str1)
-
-    items = [-0.001, -0.01, 0, 0, 0.001, 0.01, -0.1, -0.0001, 0, 0, 0.1, 0.0001]
+    # Creating mutation patterns
+    items = [-0.001, -0.01, 0, 0, 0.001, 0.01, -0.1, -0.0001, 0, 0, 0.1, 0.0001]  # Mutation types
     newI = []
-    for p in permutations(items, 4):
+    for p in permutations(items, 4):  # Permutation of mutation types
         newI.append(list(p))
     newI.sort()
-    newI = list(newI for newI, _ in itertools.groupby(newI))
-
+    newI = list(newI for newI, _ in itertools.groupby(newI))  # It creates pattern of mutations
+    # Genetic algorithm
     for epoha in range(Epos):
-        organism = [king]
+        organism = [king]  # King is the part of choose
         for x in range(Deep):
             for k in range(100):
-
+                # Secure random selection of mutation pattern, it uses os.random to make it trully random
                 secure_random = random.SystemRandom()
                 newW = [list(secure_random.choice(newI)) for k in range(4)]
-
+                # Creating new genome of specy and checking its accuracy level
                 new_weight = pd.DataFrame(np.array(newW))
                 new_weight.columns = ['reasonIN', 'reasonOUT', 'usertypeIN', 'usertypeOUT']
                 new_weight = new_weight.set_index([['reasonIN', 'reasonOUT', 'usertypeIN', 'usertypeOUT']])
@@ -159,15 +174,17 @@ def train():
                     organism.append({"Epoch": epoha, "Genome": genome, "Accuracy": float(Accuracy(genome))})
                 except:
                     organism.append({"Epoch": epoha, "Genome": genome, "Accuracy": float(0)})
-
+        # Defining its best result
         prince = max(organism, key=lambda x: x['Accuracy'])
+        # If prince`s accuracy better than king`s, then new king is here
         if float(prince["Accuracy"]) >= float(king["Accuracy"]):
+            # It writes it to kings.txt log file
             king = prince.copy()
             filename = "kings.txt"
             print("King is dead, new king is now!")
             with open(filename, 'a') as out:
                 out.write(str(king) + '\n')
-
+        # Prompt results of the algorithm
         bb = 100 * epoha / Epos
         print("Percentage is: " + str(bb))
         print("Prince accuracy is " + str(prince['Accuracy']))
@@ -179,8 +196,10 @@ def train():
         print(king["Genome"])
         print("----------------------")
         genome = king["Genome"]
+        # If accuracy is better than 98%, then -> break GA
         if king["Accuracy"] > 98:
             break
+    # Print out results
     print()
     print()
     print("=======================================================================")
@@ -200,9 +219,12 @@ def train():
         out.write(str1)
 
 
+# Function to use CBR by other part, takes case and returns decision
 def main(usertypein, usertypeout, reasonin, reasonout):
-    test = [str.lower(usertypein), str.lower(usertypeout), str.lower(reasonin), str.lower(reasonout)]
-    return test
-
-
-train()
+    weights = []  # It is the best genome found by GA (genetic algorithm)
+    new_weight = pd.DataFrame(np.array(weights))
+    new_weight.columns = ['reasonIN', 'reasonOUT', 'usertypeIN', 'usertypeOUT']
+    new_weight = new_weight.set_index([['reasonIN', 'reasonOUT', 'usertypeIN', 'usertypeOUT']])
+    test = {'output': 1, 'usertypeIN': str.lower(usertypein), 'reasonIN': str.lower(reasonin),
+            'reasonOUT': str.lower(reasonout), 'usertypeOUT': str.lower(usertypeout), 'id': 0}
+    return specy(new_weight, test)
